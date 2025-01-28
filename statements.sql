6b81acf1122bf9ab4ead185dd4ecdfd1de2caaeb
Mcorciasgelt <michael.corcias@gelt.com>
2025-01-28 10:05:55 +0100

    Avances

--- statements.sql
+++ statements.sql
@@ -1,35 +1,57 @@
 /* Relación tipo 1:1 */
 -- PASO 1
--- Tu código aquí
+CREATE TABLE usuarios (
+id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(50) NOT NULL,
+apellido VARCHAR(100) NOT NULL,
+email VARCHAR(100) NOT NULL,
+edad INT
+)
 
 
 -- PASO 2
--- Tu código aquí
+CREATE TABLE roles (
+id_rol INT AUTO_INCREMENT PRIMARY KEY,
+nombre_rol VARCHAR(50) NOT NULL
+)
 
 
 -- PASO 3
--- Tu código aquí
+ALTER TABLE usuarios ADD id_rol INT;
 
 
 -- PASO 4
--- Tu código aquí
+SELECT usuarios.id_usuario, usuarios.nombre, usuarios.apellido, usuarios.email, usuarios.edad, roles.nombre_rol
+
+FROM usuarios
+JOIN roles ON roles.id_rol = usuarios.id_rol;
 
 
 /* Relación tipo 1:N */
 -- PASO 1
--- Tu código aquí
+CREATE TABLE categorias (
+id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+nombre_categoria VARCHAR(100) NOT NULL
+)
 
 
 -- PASO 2
--- Tu código aquí
+ALTER TABLE usuarios ADD id_categoria INT;
 
 
 -- PASO 3
--- Tu código aquí
+UPDATE usuarios SET id_categoria = 1 WHERE id_usuario IN (1, 5, 9, 13, 17);
+UPDATE usuarios SET id_categoria = 2 WHERE id_usuario IN (2, 6, 10, 14, 18);
+UPDATE usuarios SET id_categoria = 3 WHERE id_usuario IN (3, 7, 11, 15, 19);
+UPDATE usuarios SET id_categoria = 4 WHERE id_usuario IN (4, 8);
+UPDATE usuarios SET id_categoria = 5 WHERE id_usuario IN (12, 16, 20);
 
 
 -- PASO 4
--- Tu código aquí
+SELECT usuarios.id_usuario, usuarios.nombre, usuarios.apellido, usuarios.email, usuarios.edad, roles.nombre_rol, categorias.nombre_categoria
+FROM usuarios
+JOIN roles ON roles.id_rol = usuarios.id_rol
+JOIN categorias ON categorias.id_categoria = usuarios.id_categoria;
 
 /* Relación tipo N:M */
 -- PASO 1
